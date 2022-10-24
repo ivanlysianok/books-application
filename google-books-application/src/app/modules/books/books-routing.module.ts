9a4cb3fd9b1a26165d4b5dba144b157d8e2d9949
Ivan Lysianok <ivanlysianok@gmail.com>
2022-10-24 21:06:20 +0200

    Stage changes

--- google-books-application/src/app/modules/books/books-routing.module.ts
+++ google-books-application/src/app/modules/books/books-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PageNotFoundComponent } from 'src/app/shared/components/page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from '../../shared/components/page-not-found/page-not-found.component';
 import { BookDetailComponent } from './components/book-detail/components/book-detail.component';
 import { BooksOverviewComponent } from './components/books-overview/components/overview-page/books-overview.component';
 

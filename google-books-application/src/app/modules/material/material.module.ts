5e7dad2b20c4b99ccb0856dac09727898e6ff892
Ivan Lysianok <ivanlysianok@gmail.com>
2023-06-17 20:26:17 +0200

    Angular 15 migration

--- google-books-application/src/app/modules/material/material.module.ts
+++ google-books-application/src/app/modules/material/material.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 
 @NgModule({
   imports: [MatButtonModule],
